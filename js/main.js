e6c88ff629b8524dd07bccef1c9f53b47fd17520
rskarthik96@gmail.com <ks@localhost.localdomain>
2019-09-16 02:11:19 +0530

    added google maps

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+// map for vadodara location
+function initMap() {
+
+    // my location
+    const loc = new google.maps.LatLng(parseFloat('22.32'), parseFloat('73.25'));
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: loc,
+        zoom: 14
+    });
+
+    const marker = new google.maps.Marker({
+        position: loc,
+        map: map
+    });
+}
+
+// smooth scrolling 
+
+$(".mainNav a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html , body').animate({
+                scrollTop: $(hash).offset().top - 45
+            },
+            600
+        );
+    }
 });
